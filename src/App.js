5df6f3cf73649f1e5eba53e6da058b8367c7772a
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-06-12 22:50:15 +0530

    chore: change format

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import "./App.css";
+import Dashboard from "./components/Dashboard.js";
 import LoginPage from "./components/LoginPage.js";
 
 function App() {
   return (
-    <div className="app">
-      <div className="container">
-        <LoginPage />
+    <>
+      <div className="app">
+        <div className="container">
+          <LoginPage />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
