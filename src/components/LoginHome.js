4b58dc1e4cfc709c1ef52c7bef719de13fc410bd
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-06-12 11:10:46 +0530

    feat: create home page section

---
+++ src/components/LoginHome.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "../styles/LoginHome.css";
+import LoginPageImage from "../assets/images/LoginPageImage.png";
+
+export default function LoginHome() {
+  return (
+    <div className="overlap-group">
+      <img src={LoginPageImage} className="clip-online" alt="Login page" />
+      <div className="register">
+        <button className="text-wrapper-4">Register</button>
+      </div>
+      <p className="lets-begin-the">
+        <span className="span">Lets begin the</span>
+        <span className="text-wrapper-5">&nbsp;</span>
+        <span className="span">
+          journey
+          <br />
+          to the{" "}
+        </span>
+        <span className="text-wrapper-6">success</span>
+      </p>
+      <p className="the-road-to-success">
+        The road to success is paved with focused effort and effective time
+        management. But let&#39;s face it, staying concentrated for long
+        stretches can be tough. That&#39;s where the Pomodoro Technique comes
+        in!
+      </p>
+    </div>
+  );
+}
