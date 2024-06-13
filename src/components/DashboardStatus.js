7d2e9d3452afe6f4ad80c8fc6fae450fc8de0611
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-06-13 23:00:28 +0530

    feat: create status bar

---
+++ src/components/DashboardStatus.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/DashboardStatus.css";
+
+export default function DashboardStatus() {
+  return (
+    <>
+      <div className="dashboard-pomodoro">
+        <a href="#">Dashboard</a> &gt; <span>Pomodoro Timer</span>
+      </div>
+    </>
+  );
+}
