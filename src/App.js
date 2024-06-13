938ef240639e5c02dda0d6c6c8f07541a3b1042a
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-06-13 23:03:17 +0530

    chore: change directry to dashboard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Dashboard from "./components/Dashboard.js";
 import LoginPage from "./components/LoginPage.js";
+import DashPomodoro from "./components/DashPomodoro.js";
 
 function App() {
   return (
