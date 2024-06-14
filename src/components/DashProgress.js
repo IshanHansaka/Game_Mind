f8ad402266dee19a58d116a3abd7af4507a1d921
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-06-14 17:42:55 +0530

    feat: create new progress component

--- src/components/DashProgress.js
+++ src/components/DashProgress.js
@@ -3,112 +3,18 @@ import "../styles/DashProgress.css";
 
 export default function DashProgress() {
   return (
-    <div className="process-monitoring">
-      <div className="div">
-        <div className="overlap">
-          <div className="game-mind">
-            <div className="text-wrapper">GAME</div>
-            <div className="text-wrapper">MIND</div>
-          </div>
-          <img className="vector" alt="Vector" src="vector.svg" />
-        </div>
-        <div className="overlap-group">
-          <div className="overlap-2">
-            <div className="rectangle" />
-            <div className="text-wrapper-2">Highest Work Duration</div>
-            <div className="text-wrapper-3">5 Hourse 22 Jan</div>
-            <div className="text-wrapper-4">&gt;</div>
-          </div>
-          <div className="group">
-            <div className="line-chart">
-              <div className="overlap-group-2">
-                <img className="img" alt="X axis grid" src="x-axis-grid.svg" />
-                <img
-                  className="x-axis-grid"
-                  alt="X axis grid"
-                  src="image.svg"
-                />
-                <img
-                  className="x-axis-grid-2"
-                  alt="X axis grid"
-                  src="x-axis-grid-2.svg"
-                />
-                <img
-                  className="x-axis-grid-3"
-                  alt="X axis grid"
-                  src="x-axis-grid-3.svg"
-                />
-                <img
-                  className="x-axis-grid-4"
-                  alt="X axis grid"
-                  src="x-axis-grid-4.svg"
-                />
-                <img
-                  className="x-axis-grid-5"
-                  alt="X axis grid"
-                  src="x-axis-grid-5.svg"
-                />
-                <img
-                  className="x-axis-grid-6"
-                  alt="X axis grid"
-                  src="x-axis-grid-6.svg"
-                />
-                <img
-                  className="y-axis-grid"
-                  alt="Y axis grid"
-                  src="y-axis-grid-2.svg"
-                />
-                <img
-                  className="y-axis-grid-2"
-                  alt="Y axis grid"
-                  src="y-axis-grid-3.svg"
-                />
-                <img
-                  className="y-axis-grid-3"
-                  alt="Y axis grid"
-                  src="y-axis-grid-4.svg"
-                />
-                <img className="img" alt="Y axis" src="y-axis.svg" />
-                <img className="line" alt="Line" src="line.svg" />
-                <img className="marker" alt="Marker" src="marker.svg" />
-                <img className="marker-2" alt="Marker" src="marker-2.svg" />
-                <img className="marker-3" alt="Marker" src="marker-3.svg" />
-                <img className="marker-4" alt="Marker" src="marker-4.svg" />
-                <img className="marker-5" alt="Marker" src="marker-5.svg" />
-                <img className="marker-6" alt="Marker" src="marker-6.svg" />
-                <img className="marker-7" alt="Marker" src="marker-7.svg" />
-              </div>
-              <div className="x-axis-wrapper">
-                <img className="x-axis" alt="X axis" src="x-axis.svg" />
-              </div>
-              <img
-                className="y-axis-grid-4"
-                alt="Y axis grid"
-                src="y-axis-grid-5.svg"
-              />
-              <div className="text-wrapper-5">20 Jan</div>
-              <div className="text-wrapper-6">21 Jan</div>
-              <div className="text-wrapper-7">22 Jan</div>
-              <div className="text-wrapper-8">23 Jan</div>
-              <div className="text-wrapper-9">Date</div>
-              <div className="text-wrapper-10">Work Hours</div>
-              <div className="text-wrapper-11">24 Jan</div>
-              <div className="text-wrapper-12">25 Jan</div>
-              <div className="text-wrapper-13">26 Jan</div>
-              <div className="text-wrapper-14">6</div>
-              <div className="text-wrapper-15">7</div>
-              <div className="text-wrapper-16">8</div>
-              <div className="text-wrapper-17">9</div>
-              <div className="text-wrapper-18">10</div>
-            </div>
-          </div>
-        </div>
-        <div className="dashboard-process-wrapper">
-          <div className="dashboard-process">
-            Dashboard &gt; Process Monitoring
+    <>
+      <div className="pro-overlap">
+        <div className="pro-chart" />
+        <div className="pro-rectangle">
+          <div className="pro-text-wrapper-2">Highest Work Duration</div>
+          <div className="pro-text-wrapper-4">&gt;</div>
+          <div className="pro-text-wrapper-3">
+            <span className="pro-time">5</span> Hours,{" "}
+            <span className="pro-date">22 January</span>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
