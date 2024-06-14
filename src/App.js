8353fa9d57eb2c4c63fe8c33dd5b8813563421ab
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-06-14 14:02:03 +0530

    feat: add new component paths

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import Dashboard from "./components/Dashboard.js";
 import LoginPage from "./components/LoginPage.js";
 import DashPomodoro from "./components/DashPomodoro.js";
+import Register from "./components/Register.js";
+import Signin from "./components/Signin.js";
 
 function App() {
   return (
     <>
-      <Dashboard />
+      <LoginPage />
     </>
   );
 }
