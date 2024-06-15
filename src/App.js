a5693a50f73f4756f8f1d5f556770166a4d13173
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-06-15 11:36:50 +0530

    feat: import LoginPage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard.js";
-
+import LoginPage from "./pages/LoginPage.js";
 
 function App() {
   return (
