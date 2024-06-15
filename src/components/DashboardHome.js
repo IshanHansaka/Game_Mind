8cf42ae011ff12d6fa5b3df02c37f9dcca996d58
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-06-15 11:23:16 +0530

    chore: use sapn and div tags to link components instead of a tags

--- src/components/DashboardHome.js
+++ src/components/DashboardHome.js
@@ -6,7 +6,13 @@ import Buzzwire from "../assets/images/buzzwire.svg";
 import Ellipse1 from "../assets/images/ellipse 1.png";
 import Ellipse2 from "../assets/images/ellipse 2.png";
 
-export default function DashboardHome() {
+export default function DashboardHome({
+  onSettingsClick,
+  onStatsClick,
+  onDanceClick,
+  onMemoryClick,
+  onBuzzClick,
+}) {
   return (
     <>
       <div className="overlap-2 home-tiles">
@@ -15,40 +21,59 @@ export default function DashboardHome() {
           <img className="ellipse" alt="Ellipse2" src={Ellipse2} />
           <div className="text-wrapper-02">71%</div>
         </div>
-        <div className="text-wrapper-03">Process Monitoring</div>
+        <div className="text-wrapper-03">Progress Monitoring</div>
         <p className="p">
-          <span className="span">
-            <a href="#">view status</a>
+          <span
+            className="span"
+            role="button"
+            tabIndex={0}
+            onClick={onStatsClick}
+          >
+            view stats
           </span>
         </p>
       </div>
       <div className="overlap-4 home-tiles">
         <p className="p">
-          <span className="span">
-            <a href="#">settings</a>
+          <span
+            className="span"
+            role="button"
+            tabIndex={0}
+            onClick={onSettingsClick}
+          >
+            settings
           </span>
         </p>
         <div className="text-wrapper-05">25 : 00</div>
         <div className="text-wrapper-06">Pomodoro Timer</div>
       </div>
-      <a href="#">
-      <div className="overlap-5 game-tiles">
+      <div
+        className="overlap-5 game-tiles"
+        role="button"
+        tabIndex={0}
+        onClick={onBuzzClick}
+      >
         <div className="text-wrapper-07">Buzzwire Game</div>
         <img className="ram-memory" alt="Ram memory" src={Buzzwire} />
       </div>
-      </a>
-      <a href="#">
-        <div className="overlap-6 game-tiles">
-          <div className="text-wrapper-07">Dancing Game</div>
-          <img className="grade" alt="Grade" src={Dancingpad} />
-        </div>
-      </a>
-      <a href="#">
-      <div className="overlap-7 game-tiles">
+      <div
+        className="overlap-6 game-tiles"
+        role="button"
+        tabIndex={0}
+        onClick={onDanceClick}
+      >
+        <div className="text-wrapper-07">Dancing Game</div>
+        <img className="grade" alt="Grade" src={Dancingpad} />
+      </div>
+      <div
+        className="overlap-7 game-tiles"
+        role="button"
+        tabIndex={0}
+        onClick={onMemoryClick}
+      >
         <div className="text-wrapper-07">Memory Game</div>
         <img className="element-f" alt="Element f" src={Memorygame} />
       </div>
-      </a>
     </>
   );
 }
