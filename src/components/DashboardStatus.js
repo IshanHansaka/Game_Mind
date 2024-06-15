09bbd5147ed3a1164553f20347a89865476e2bd3
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-06-15 11:24:27 +0530

    feat: add backward link to dashboard button and implement usestate to substatus change dynamically changes when components change

--- src/components/DashboardStatus.js
+++ src/components/DashboardStatus.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/DashboardStatus.css";
 
-export default function DashboardStatus() {
+export default function DashboardStatus({ onDashClick, currentComponent }) {
+  const renderStatus = () => {
+    switch (currentComponent) {
+      case "pomodoro":
+        return "Pomodoro Timer";
+      case "progress":
+        return "Progress Monitoring";
+      case "dance":
+        return "Dancing Game";
+      case "memory":
+        return "Memory Game";
+      case "buzz":
+        return "Buzzwire Game";
+      default:
+        return "";
+    }
+  };
   return (
     <>
       <div className="dashboard-pomodoro">
-        <a href="#">Dashboard</a> &gt; <span>Pomodoro Timer</span>
+        <span
+          className="status-span"
+          onClick={onDashClick}
+          role="button"
+          tabIndex={0}
+        >
+          Dashboard
+        </span>{" "}
+        &gt; <span>{renderStatus()}</span>
       </div>
     </>
   );
