6535b542bccee1771db024f0ffe7b48b9d3d9dc0
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-06-17 22:30:10 +0530

    feat: change components accourding to states

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,19 +1,40 @@
-import React from "react";
-import LoginNavbar from "../components/LoginNavbar";
-import LoginHome from "../components/LoginHome";
+import React, { useState } from "react";
+import LoginNavbar from "../components/LoginNavbar.js";
+import LoginHome from "../components/LoginHome.js";
+import Signin from "../components/Signin.js";
+import Register from "../components/Register.js";
 import "../styles/LoginPage.css";
 
 export default function LoginPage() {
-  return (
-    <div className="login">
-      <div className="container">
-        <div className="overlap">
-          <LoginNavbar />
-        </div>
-        <div className="overlap-group">
-          <LoginHome />
+  const [currentComponent, setCurrentComponent] = useState("home");
+
+  const handleSignin = () => {
+    setCurrentComponent("signin");
+  };
+
+  const handleRegster = () => {
+    setCurrentComponent("register");
+  };
+
+  let RenderedComponent;
+  if (currentComponent === "home") {
+    RenderedComponent = (
+      <div className="login">
+        <div className="container">
+          <div className="overlap">
+            <LoginNavbar onSignin={handleSignin}/>
+          </div>
+          <div className="overlap-group">
+            <LoginHome onRegister={handleRegster}/>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else if (currentComponent === "signin") {
+    RenderedComponent = <Signin onRegister={handleRegster}/>;
+  } else if (currentComponent === "register") {
+    RenderedComponent = <Register onSignin={handleSignin}/>;
+  }
+
+  return <>{RenderedComponent}</>;
 }
