c4c216fcf1b9c2faacb1e63374e38bd21c3c9d93
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-06-17 22:27:58 +0530

    feat: add signout

--- src/components/DashboardNavbar.js
+++ src/components/DashboardNavbar.js
@@ -1,17 +1,34 @@
 import React from "react";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase/firebaseConfig.js"
 import ProfilePicture from "../assets/images/profilepicture.png";
 import "../styles/DashboardNavbar.css";
 
 export default function DashboardNavbar() {
+  const handleSignout = () => {
+    signOut(auth)
+      .then(() => {
+        localStorage.clear();
+        window.location.reload();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <>
       <div className="game-mind-0">
         <div className="text-wrapper-0">GAME</div>
         <div className="text-wrapper-0">MIND</div>
       </div>
-      <a href="#">
+      <div
+        className="profile-pic"
+        role="button"
+        tabIndex={0}
+        onClick={handleSignout}
+      >
         <img className="vector" alt="Vector" src={ProfilePicture} />
-      </a>
+      </div>
     </>
   );
 }
