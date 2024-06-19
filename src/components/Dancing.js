88db9d54c34cafbcd8dda0dc2e4217bfd66e3352
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-06-19 22:58:41 +0530

    feat: add web sockets to communicate with esp32

--- src/components/Dancing.js
+++ src/components/Dancing.js
@@ -1,16 +1,72 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/Dancing.css";
 
 export default function Dancing() {
+  const [socket, setSocket] = useState(null);
+  const integers = [0, 1, 2, 3, 0, 2, 1, 3, 0, 1];
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [visibleIndex, setVisibleIndex] = useState(null);
+
+  useEffect(() => {
+    const ws = new WebSocket("ws://192.168.8.116:81");
+    setSocket(ws);
+
+    ws.onopen = () => {
+      console.log("WebSocket Client Connected");
+    };
+
+    ws.onmessage = (message) => {
+      console.log("Received: " + message.data);
+    };
+
+    ws.onerror = (error) => {
+      console.error("WebSocket Error: ", error);
+    };
+
+    ws.onclose = () => {
+      console.log("WebSocket Connection Closed");
+    };
+
+    return () => {
+      ws.close();
+    };
+  }, []);
+
+  const sendRandomInteger = () => {
+    if (socket && socket.readyState === WebSocket.OPEN) {
+      const nextInteger = integers[currentIndex];
+      socket.send(nextInteger.toString());
+      setVisibleIndex(nextInteger);
+      console.log("Sent: " + nextInteger);
+      setCurrentIndex((currentIndex + 1) % integers.length);
+    } else {
+      console.log("WebSocket is not connected");
+    }
+  };
+
+  const polygons = [
+    { points: "200,50 350,350 50,350", className: "up" },
+    { points: "50,200 350,50 350,350", className: "left" },
+    { points: "200,350 50,50 350,50", className: "down" },
+    { points: "350,200 50,350 50,50", className: "right" },
+  ];
   return (
     <div className="danc-container">
       <div className="danc-rectangle-2">
         <div className="danc-rectangle-3">
-             
+          <svg width="400" height="400">
+            {polygons.map((polygon, index) => (
+              <polygon
+                key={index}
+                points={polygon.points}
+                style={{ display: visibleIndex === index ? "block" : "none" }}
+              />
+            ))}
+          </svg>
         </div>
         <div className="danc-rectangle-4">
           <div className="danc-div-wrapper">
-            <button className="danc-startbtn">
+            <button className="danc-startbtn" onClick={sendRandomInteger}>
               <div className="danc-text-wrapper-2">Start</div>
             </button>
           </div>
