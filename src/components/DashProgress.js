d99edac6b7fdd30440311c94f5d73d22276a601c
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-06-20 22:27:44 +0530

    feat: add three charts

--- src/components/DashProgress.js
+++ src/components/DashProgress.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "../styles/DashProgress.css";
+import ProgressChartArea from "./ProgressChartArea.js";
+import ProgressChartBar from "./ProgressChartBar.js";
+import ProgressChartLine from "./ProgressChartLine.js";
 
 export default function DashProgress() {
   return (
     <>
       <div className="pro-overlap">
-        <div className="pro-chart" />
+        <div className="pro-chart">
+          <div className="chart-container">
+            <ProgressChartBar />
+          </div>
+        </div>
         <div className="pro-rectangle">
           <div className="pro-text-wrapper-2">Highest Work Duration</div>
           <div className="pro-text-wrapper-4">&gt;</div>
