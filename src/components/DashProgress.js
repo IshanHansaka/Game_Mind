c731cb125c1f965c3c40aa280d876d1b8044bb94
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-06-21 10:18:12 +0530

    chore: remove heighest work  hours section

--- src/components/DashProgress.js
+++ src/components/DashProgress.js
@@ -12,15 +12,7 @@ export default function DashProgress() {
           <div className="chart-container">
             <ProgressChartBar />
           </div>
-        </div>
-        <div className="pro-rectangle">
-          <div className="pro-text-wrapper-2">Highest Work Duration</div>
-          <div className="pro-text-wrapper-4">&gt;</div>
-          <div className="pro-text-wrapper-3">
-            <span className="pro-time">5</span> Hours,{" "}
-            <span className="pro-date">22 January</span>
-          </div>
-        </div>
+        </div> 
       </div>
     </>
   );
