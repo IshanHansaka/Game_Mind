86aeb199305973e09dd473bff435cb6320bab340
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-06-21 10:20:07 +0530

    feat: represent firestore data in recharts

--- src/components/ProgressChartArea.js
+++ src/components/ProgressChartArea.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   AreaChart,
   Area,
@@ -8,45 +8,31 @@ import {
   CartesianGrid,
   Legend,
 } from "recharts";
+import { db } from "../firebase/firebaseConfig";
+import { collection, collectionGroup, getDocs } from "firebase/firestore";
 
 export default function ProgressChartArea() {
-  const productSales = [
-    {
-      name: "Jan",
-      product1: 4000,
-      product2: 2400,
-    },
-    {
-      name: "Feb",
-      product1: 3000,
-      product2: 2210,
-    },
-    {
-      name: "Mar",
-      product1: 2000,
-      product2: 2290,
-    },
-    {
-      name: "Apr",
-      product1: 2780,
-      product2: 2000,
-    },
-    {
-      name: "May",
-      product1: 1890,
-      product2: 2181,
-    },
-    {
-      name: "Jun",
-      product1: 2390,
-      product2: 2500,
-    },
-  ];
+  const [salesData, setSalesData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const collectionRef = collection(db, "test");
+      try {
+        const snapshot = await getDocs(collectionRef);
+        const data = snapshot.docs.map((doc) => ({ ...doc.data() }));
+        setSalesData(data.reverse());
+      } catch (err) {
+        console.error("Error fetching data:", err.message);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <>
       <ResponsiveContainer>
-        <AreaChart data={productSales}>
+        <AreaChart data={salesData}>
           <XAxis
             dataKey="name"
             tick={{ fontSize: 20, fill: "#000000" }}
