eaf790be54d2c1fa0ae8992d2bc81321d647175d
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-07-26 21:34:13 +0530

    chore: change code format

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,13 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { getFirestore } from "firebase/firestore";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAxgTKliDO4MNop_gZEH_led3kpI2MlfBs",
   authDomain: "first-year-hardware-project.firebaseapp.com",
